8881c65cb2cdd9e7490332713bd9e406eb92ddb3
myagmb28 <a01077066512@gmail.com>
2024-03-20 19:51:22 +0900

    a

---
+++ idk/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//      int[] array = { 1, 2, 3 };
+//        System.out.println(array[0]);
+//        Scanner scanner = new Scanner(System.in);
+
+
+        
+    }
+}
+
+class Student {
+    String name;
+    int grade;
+}
